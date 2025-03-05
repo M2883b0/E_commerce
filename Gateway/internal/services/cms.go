fb67f214ea3d73a84ab7f124b7dbcfec999630b8
M2883b0 <64240403+M2883b0@users.noreply.github.com>
2025-03-05 12:31:29 +0800

    Update cms.go

--- Gateway/internal/services/cms.go
+++ Gateway/internal/services/cms.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	ai "Gateway/internal/api/ai_agent"
 	"Gateway/internal/api/cart"
 	"Gateway/internal/api/checkout"
 	"Gateway/internal/api/operate"
@@ -22,6 +23,7 @@ type CmsAPP struct {
 	cartServiceClient     cart.CartServiceClient
 	checkoutServiceClient checkout.CheckoutServiceClient
 	paymentServiceClient  payment.PaymentServiceClient
+	aiAgentClient         ai.AiAgentClient
 }
 
 func NewCmsApp() *CmsAPP {
@@ -33,6 +35,7 @@ func NewCmsApp() *CmsAPP {
 	connCartServiceClient(app)
 	connCheckoutServiceClient(app)
 	connPaymentServiceClient(app)
+	connAIAgentClient(app)
 	return app
 }
 
@@ -269,6 +272,42 @@ func connPaymentServiceClient(app *CmsAPP) {
 
 	//endpoint := "discovery:///provider"
 	endpoint := "discovery:///payment_service" //把etcd的Name标识符拿过来，找到对应的服务ip
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		//grpc.WithEndpoint("127.0.0.1:9000"),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+		),
+		//上面127.0.0.1:9000写死的，后面引入etcd服务发现。把服务注册到etcd中,端口自动分配
+		//我们的服务，现在微服务化了，有很多的分布式节点，需要etcd帮我们存储现在所有的分布式节点
+		//实现负载均衡的能力，指定请求的节点，每个请求路由到不同的机器节点上
+		grpc.WithEndpoint(endpoint),
+		grpc.WithDiscovery(dis), //服务的发现
+	)
+	if err != nil {
+		panic(err)
+	}
+	appClient := payment.NewPaymentServiceClient(conn)
+	app.paymentServiceClient = appClient
+}
+
+func connAIAgentClient(app *CmsAPP) {
+	// new etcd client
+	ETCD_ADDRR := os.Getenv("ETCD_ADDR")
+	if ETCD_ADDRR == "" {
+		ETCD_ADDRR = "127.0.0.1:2379"
+	}
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{ETCD_ADDRR}, //从etcd中，服务的发现
+	})
+	if err != nil {
+		panic(err)
+	}
+	// new dis with etcd client
+	dis := etcd.New(client)
+
+	//endpoint := "discovery:///provider"
+	endpoint := "discovery:///ai_agent" //把etcd的Name标识符拿过来，找到对应的服务ip
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		//grpc.WithEndpoint("127.0.0.1:9000"),
