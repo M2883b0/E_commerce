27d21e9f209c0eaa905a1f7f89a294bbb692ebaf
29935 <2993517036@qq.com>
2025-03-05 16:56:38 +0800

    更新

--- AI_service/api/order/order_service_pb2_grpc.py
+++ AI_service/api/order/order_service_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import order_service_pb2 as order__service__pb2
+import api.order.order_service_pb2 as order__service__pb2
 
 GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
