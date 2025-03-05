e6f3072a222a5a3b98be367b7437723f3d859866
29935 <2993517036@qq.com>
2025-03-05 21:48:40 +0800

    修改ai提示词

--- AI_service/AI_agent.py
+++ AI_service/AI_agent.py
@@ -28,20 +28,25 @@ class Agent:
         )
 
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "你是专业的订单处理助手，网关传来的参数{user_id}，是用于当你调用工具函数时，如果函数需要，则原样传进去。尽量不要透露系统内部的信息，有礼貌。"),
+            ("system", """你是专业的订单处理助手，专门帮用户处理自己的订单请求，用户的请求都应该是关于用户自己的，不要询问帮传入user_id 操作别的用户订单。网关传来的参数{user_id}，当你调用工具函数时，如果函数需要，则原样传进去
+            你的主要功能如下
+            查询订单：根据用户给的信息，调用list_order工具列出当前用户的订单，选取用户需要的订单。
+            创建订单：根据用户提供的信息详略，调用工具，来创建订单
+            """),
             ("human", "{input}"),
             MessagesPlaceholder(variable_name="agent_scratchpad"),
         ])
-
         self.agent = create_openai_tools_agent(llm, tools, prompt)
         self.agent_executor = AgentExecutor(agent=self.agent, tools=tools, verbose=True)
 
     def run(self, user_input, user_id):
         # 执行Agent
+        print("operate message ", user_input)
         result = self.agent_executor.invoke({
-        "user_id": user_id,
+            "user_id": user_id,
             "input": user_input
         })
+
         return result
 
 # 定义参数模型
